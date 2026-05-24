7c048a67942b1b2175ae9a68a2d178b60c9720f8
abu-Hadia <elearningdevops@gmail.com>
2026-05-24 14:13:42 +0300

    updated

---
+++ 08_basic_const/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const tutorial = "Go Basics"
+	const discount = 299.9
+	const maxuploadFile = 100
+	fmt.Println(tutorial, discount, maxuploadFile)
+}
