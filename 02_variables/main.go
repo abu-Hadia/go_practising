71c3e338a8c4e63aaf0a0c1459f18030ff3a5cc7
abu-Hadia <elearningdevops@gmail.com>
2026-05-24 08:38:23 +0300

    small calculation

--- 02_variables/main.go
+++ 02_variables/main.go
@@ -11,12 +11,12 @@ func main() {
 	// fmt.Println("Cash:", cash)
 
 	/// create a variable and assign a value at the same time
-	Name := "Ahmed"
-	age := 40.4
-	fmt.Println("Name:", Name)
-	fmt.Println("Age", age)
+	// Name := "Ahmed"
+	// age := 40.4
+	// fmt.Println("Name:", Name)
+	// fmt.Println("Age", age)
 
-	//Grouped variables
+	// Grouped variables
 	// var (
 	// 	JName  string = "jama"
 	// 	Salary int    = 4000
@@ -28,11 +28,26 @@ func main() {
 
 	///ANOTHER TYPE YOU CAN DECLARE VAR
 
-	subscriber := 400 //----infer  it detects automatically the data type of the variable.
-	subscriber = subscriber + 1000
+	// subscriber := 400 //----infer  it detects automatically the data type of the variable.
+	// subscriber = subscriber + 1000
 
-	product, service := "sugar", 40
+	// product, service := "sugar", 40
 
-	fmt.Println(subscriber, product, service)
+	// fmt.Println(subscriber, product, service)
+	// numb2 := 300
+	// number3 := 40
+	// fmt.Println("Total is:", numb2+number3)
+
+	//small calculation
+
+	num1 := 30
+	num2 := 40
+	total := num1 * num2
+	fmt.Println("Total is:", total)
+	if total > 1000 {
+		fmt.Println("Total is greater than 1000:", total)
+	} else {
+		fmt.Println("Total is less than 1000 so division Result is:", float64(num1)/float64(num2))
+	}
 
 }
