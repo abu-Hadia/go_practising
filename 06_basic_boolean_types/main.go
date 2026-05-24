4e4ceb641e8cad9e806ed74f2e1c52f39dd02b83
abu-Hadia <elearningdevops@gmail.com>
2026-05-24 13:56:44 +0300

    small change

--- 06_basic_boolean_types/main.go
+++ 06_basic_boolean_types/main.go
@@ -5,13 +5,20 @@ import "fmt"
 func main() {
 
 	//boolean
-	isLogging := true
-	hasSubscription := true
-	isAdmin := true
+	// isLogging := true
+	// hasSubscription := true
+	// isAdmin := true
 
-	cancancelpost := isAdmin && hasSubscription
-	canupdate := isLogging || isAdmin
-	fmt.Println("you can cancel:", cancancelpost)
-	fmt.Println("you can update it:", canupdate)
+	// cancancelpost := isAdmin && hasSubscription
+	// canupdate := isLogging || isAdmin
+	// fmt.Println("you can cancel:", cancancelpost)
+	// fmt.Println("you can update it:", canupdate)
+	var Age int = 29
+	Adult := Age > 13
+	if Adult {
+		fmt.Println("adult is eligible to delete file:", Adult)
+	} else {
+		fmt.Println("you are not eligible to delete file")
+	}
 
 }
