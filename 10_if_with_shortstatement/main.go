55c5a8b51918c7afdacea9fbc8c3d1348585d559
abu-Hadia <elearningdevops@gmail.com>
2026-05-25 07:29:09 +0300

    ifelsestatement

--- 10_if_with_shortstatement/main.go
+++ 10_if_with_shortstatement/main.go
@@ -3,14 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	items := 6
-	priceItem := 10
-	if total := items * priceItem; total >= 60 {
-		fmt.Println("expensive", total)
-	} else if total >= 40 {
-		fmt.Println("its Ok")
-	} else {
-		fmt.Println("its cheap mansha allah")
+	// items := 6
+	// priceItem := 10
+	// if total := items * priceItem; total >= 60 {
+	// 	fmt.Println("expensive", total)
+	// } else if total >= 40 {
+	// 	fmt.Println("its Ok")
+	// } else {
+	// 	fmt.Println("its cheap mansha allah")
+	// }
+
+	a := 10
+	b := 4
+	sum := a + b
+	multi := a * b
+	divide := a / b
+	if sum > 0 {
+		fmt.Println("sum is", sum)
+	} else if multi < 40 {
+		fmt.Println("multiple:", multi)
+	} else if divide > 1 {
+		fmt.Println("divide is:", divide)
 	}
 
 }
