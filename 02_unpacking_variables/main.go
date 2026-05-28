30aa5ac8fa2641f616393292487d01486009d6e7
abu-Hadia <elearningdevops@gmail.com>
2026-05-28 18:21:12 +0300

    multiple variables

--- 02_unpacking_variables/main.go
+++ 02_unpacking_variables/main.go
@@ -8,6 +8,10 @@ func main() {
 
 	fmt.Println(myvar)
 	myfunnc()
+
+	// multiple variable declarateion
+	var year, age, name = 2026, 30, "Ahmed"
+	fmt.Println(year, name, age)
 }
 
 func myfunnc() {
