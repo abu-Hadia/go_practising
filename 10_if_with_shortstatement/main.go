dedacf96bfcc112557d76599d36c5483ad12e428
abu-Hadia <elearningdevops@gmail.com>
2026-05-28 16:20:29 +0300

    last update

--- 10_if_with_shortstatement/main.go
+++ 10_if_with_shortstatement/main.go
@@ -1,29 +1,130 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	// items := 6
+// 	// priceItem := 10
+// 	// if total := items * priceItem; total >= 60 {
+// 	// 	fmt.Println("expensive", total)
+// 	// } else if total >= 40 {
+// 	// 	fmt.Println("its Ok")
+// 	// } else {
+// 	// 	fmt.Println("its cheap mansha allah")
+// 	// }
+
+// 	a := 10
+// 	b := 4
+// 	sum := a + b
+// 	multi := a * b
+// 	divide := a / b
+// 	if sum > 0 {
+// 		fmt.Println("sum is", sum)
+// 	} else if multi < 40 {
+// 		fmt.Println("multiple:", multi)
+// 	} else if divide > 1 {
+// 		fmt.Println("divide is:", divide)
+// 	}
+
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// func main() {
+
+// 	var num1, num2 float64
+// 	var operator string
+
+// 	//input
+// 	fmt.Println("Enter first Number:")
+// 	fmt.Scan(&num1)
+// 	fmt.Println("Enter the operator")
+// 	fmt.Scan(&operator)
+// 	fmt.Println("Enter the second Number:")
+// 	fmt.Scan(&num2)
+
+// 	//process
+// 	// if operator == "+" {
+// 	// 	fmt.Println("Addition is:", num1+num2)
+// 	// } else if operator == "-" {
+// 	// 	fmt.Println("substraction is:", num1-num2)
+// 	// } else if operator == "*" {
+// 	// 	fmt.Println("multiplication is:", num1*num2)
+// 	// } else if operator == "/" {
+// 	// 	fmt.Println("division is:", num1/num2)
+// 	// } else {
+// 	// 	fmt.Println("invalid format")
+// 	// }
+
+// 	switch operator {
+// 	case "+":
+// 		fmt.Println(num1, "+", num2, "=", num1+num2)
+
+// 	case "-":
+// 		fmt.Println(num1, "-", num2, "=", num1-num2)
+// 	case "*":
+// 		fmt.Println(num1, "*", num2, "=", num1*num2)
+// 	case "/":
+// 		if num2 == 0 {
+// 			fmt.Println("division by zero is not allowed")
+// 		} else {
+// 			fmt.Println(num1, "/", num2, "=", num1/num2)
+// 		}
+// 	default:
+// 		fmt.Println("invalid operator")
+// 	}
+
+// }
 
 func main() {
-	// items := 6
-	// priceItem := 10
-	// if total := items * priceItem; total >= 60 {
-	// 	fmt.Println("expensive", total)
-	// } else if total >= 40 {
-	// 	fmt.Println("its Ok")
-	// } else {
-	// 	fmt.Println("its cheap mansha allah")
-	// }
 
-	a := 10
-	b := 4
-	sum := a + b
-	multi := a * b
-	divide := a / b
-	if sum > 0 {
-		fmt.Println("sum is", sum)
-	} else if multi < 40 {
-		fmt.Println("multiple:", multi)
-	} else if divide > 1 {
-		fmt.Println("divide is:", divide)
+	for {
+		var num1, num2 float64
+		var operator string
+
+		//input
+		fmt.Println("Enter first Number:")
+		fmt.Scan(&num1)
+		fmt.Println("Enter the operator")
+		fmt.Scan(&operator)
+		fmt.Println("Enter the second Number:")
+		fmt.Scan(&num2)
+
+		//process
+		// if operator == "+" {
+		// 	fmt.Println("Addition is:", num1+num2)
+		// } else if operator == "-" {
+		// 	fmt.Println("substraction is:", num1-num2)
+		// } else if operator == "*" {
+		// 	fmt.Println("multiplication is:", num1*num2)
+		// } else if operator == "/" {
+		// 	fmt.Println("division is:", num1/num2)
+		// } else {
+		// 	fmt.Println("invalid format")
+		// }
+
+		switch operator {
+		case "+":
+			fmt.Println(num1, "+", num2, "=", num1+num2)
+
+		case "-":
+			fmt.Println(num1, "-", num2, "=", num1-num2)
+		case "*":
+			fmt.Println(num1, "*", num2, "=", num1*num2)
+		case "/":
+			if num2 == 0 {
+				fmt.Println("division by zero is not allowed")
+			} else {
+				fmt.Println(num1, "/", num2, "=", num1/num2)
+			}
+		default:
+			fmt.Println("invalid operator")
+		}
+		fmt.Println()
 	}
 
 }
--- 11_for_classic_loop/main.go
+++ 11_for_classic_loop/main.go
@@ -4,12 +4,51 @@ import "fmt"
 
 func main() {
 
-	for i := 1; i <= 40; i++ {
-		if i < 40 {
-			fmt.Println("soo good", i)
-		} else {
-			fmt.Println(i)
+	// for i := 1; i <= 40; i++ {
+	// 	if i < 40 {
+	// 		fmt.Println("soo good", i)
+	// 	} else {
+	// 		fmt.Println("stop iteration")
+	// 	}
+	// }
+
+	// For LOOP WHICH PRINT *
+
+	// for i := 1; i <= 10; i++ {
+	// 	for j := 1; j <= i; j++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	//REVERSE FROM LARGE TO SMALL
+	// for i := 10; i >= 1; i-- {
+	// 	for j := 1; j <= i; j++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// loop for S
+
+	n := 7
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+
+			// top row
+			if i == 0 ||
+				// middle
+				i == n/2 ||
+				//upper right side
+				(i < n/2 && j == 0) ||
+				// lower right side
+				(i > n/2 && j == n-1) {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+
 		}
 	}
-
 }
