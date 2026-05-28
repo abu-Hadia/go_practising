04277899937f10ca27431dc103f5d57ca7700169
abu-Hadia <elearningdevops@gmail.com>
2026-05-28 17:18:01 +0300

    some data types and what are used for

--- 05_Basic_Int_types/main.go
+++ 05_Basic_Int_types/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	views1 := 100
@@ -18,4 +20,50 @@ func main() {
 	avgfloat := (float1 + float2) / 2
 	fmt.Println(avgfloat)
 
+	/// other data types
+
+	var positivevalues uint8
+	positivevalues = 100
+	fmt.Println(positivevalues)
+
+	/// negative data type
+
+	var negativevalues int8
+	negativevalues = -100
+	fmt.Println(negativevalues)
+
+	// typecase
+	var mybye byte
+	mybye = 'd'
+	fmt.Println(mybye)
+
+	// rune is alias of int8
+
+	// Signed integers (can be negative and positive)
+	// Type	Size	Range
+	// int8	8-bit	-128 to 127
+	// int16	16-bit	-32,768 to 32,767
+	// int32	32-bit	~ -2B to 2B
+	// int64	64-bit	very large range
+
+	// 	Unsigned integers (only positive, no negatives)
+	// Type	Size	Range
+	// uint8	8-bit	0 to 255
+	// uint16	16-bit	0 to 65,535
+	// uint32	32-bit	0 to ~4B
+	// uint64	64-bit	very large
+	// 	int
+	// Size depends on your system:
+	// 32-bit system → same as int32
+	// 64-bit system → same as int64
+	// 👉 Most commonly used integer
+	// uint
+	// Same as int, but only positive
+	// 👉 Avoid unless you really need unsigned behavior
+	// 🔤 rune
+	// Alias for int32
+	// Used to represent Unicode characters
+
 }
+
+// 65104109101100
