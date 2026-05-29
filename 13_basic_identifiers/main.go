c5bbc95a8b420ba153090bd37ab471b340c6a0ab
abu-Hadia <elearningdevops@gmail.com>
2026-05-29 09:44:41 +0300

    remainder

---
+++ 13_basic_identifiers/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+	// What is an identifier?
+
+	// An identifier is a name used to identify:
+
+	// variables
+	// constants
+	// functions
+	// types
+	// structs
+	// packages
+	// parameters
+
+	// Example:
+
+	// var age = 25
+
+	// 👉 age is an identifier
+
+}
---
+++ 14_basisc_operators/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a, b int = 35, 3
+	var c int
+	c = a % b
+	fmt.Println(c)
+
+}
